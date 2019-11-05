246dfdb283919e2120a44d143d1fd97ffe30a4e6
MohamedElm1678 <Mohamedapple@hotmail.com>
2019-11-05 15:23:55 -0800

    Add some actions

---
+++ src/main/java/com/frc1678/c2019/auto/actions/LambdaAction.java
@@ -0,0 +1,30 @@
+package com.frc1678.c2019.auto.actions;
+
+public class LambdaAction implements Action {
+
+    public interface VoidInterace {
+        void f();
+    }
+
+    VoidInterace mF;
+
+    public LambdaAction(VoidInterace f) {
+        this.mF = f;
+    }
+
+    @Override
+    public void start() {
+        mF.f();
+    }
+
+    @Override
+    public void update() {}
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public void done() {}
+}
