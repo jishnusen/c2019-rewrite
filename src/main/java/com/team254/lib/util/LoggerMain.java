75e97500a1f4f4d0987add84a854f4887ccc2684
EithneA-V <eithne.arsuaga.vazquez@gmail.com>
2019-12-05 21:32:22 -0800

    Add comment to LoggerMain

--- src/main/java/com/team254/lib/util/LoggerMain.java
+++ src/main/java/com/team254/lib/util/LoggerMain.java
@@ -4,7 +4,7 @@ public class LoggerMain {
         ILoggable loggable = new TestLoggable(); 
         float[] get_items = loggable.get_items();
         String[] get_item_names = loggable.get_item_names();
-        System.out.println(get_items[0]);
+        System.out.println(get_items[0]); //println will be changed to files --> write stuff to file
         System.out.println(get_item_names[0]);
     }
  }
