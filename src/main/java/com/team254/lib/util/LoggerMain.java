e10b990267c890fbc43656f805a408db2c5b59a3
EithneA-V <eithne.arsuaga.vazquez@gmail.com>
2020-01-02 09:56:31 -0800

    Rename Logger.java to LoggingSystem.java for compatatbility with class name

--- src/main/java/com/team254/lib/util/LoggerMain.java
+++ src/main/java/com/team254/lib/util/LoggerMain.java
@@ -1,38 +1,7 @@
-//  main logging function 
-//  import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import java.util.ArrayList;
-import java.io.FileWriter; 
+package com.team254.lib.util; 
+
 public class LoggerMain {
     public static void main(String[] args) {
-        // creating a loggable object 
-        ILoggable loggable = new TestLoggable();
-        //  creating a usingFileWriter object
-        try{
-        FileWriter fileWriter = new FileWriter("Test.csv");
-        //  LoggableItems object
-        ArrayList<ILoggable> loggable_items = new ArrayList<ILoggable>();
-        //  adding Loggable to Loggable_items list
-        loggable_items.add(loggable);
-        //  for loop will loop over anything that implements the logger
-        for (int i=0; i < loggable_items.size(); i++) {
-           ArrayList<Double> items = loggable_items.get(i).get_items();
-           ArrayList<String> item_names = loggable_items.get(i).get_item_names();
-           //  assertArrayEquals(items[i], item_names[i]);
-           //  write to files
-           // fileWriter.write(items);
-           //  fileWrter.write(item_names);
-           for (int h=0; h < item_names.size(); h++) {
-           fileWriter.write(item_names.get(h));
-           fileWriter.write(",");
-           }
-           fileWriter.write("\n");
-           for (int j=0; j < items.size(); j++) {
-            fileWriter.write(items.get(j).toString());
-            fileWriter.write(",");
-           } 
-           fileWriter.write("\n");
-        }
-        fileWriter.close(); // makes sure its actually written
-    } catch (Exception e) {}  // making compiler happy by trying to catch stuff that could crash 
+      LoggingSystem LS = new LoggingSystem();
     }
  }
---
+++ src/main/java/com/team254/lib/util/LoggingSystem.java
@@ -0,0 +1,61 @@
+/* real logger code
+  TODO (EithneA-V)
+    1) Create Logging System class
+        1.1) Constructor function (base class pointer)
+        1.2) Member var that is a list of obj to call when logging
+            1.2.1) List of things it can call (and that can call it --> same that it can call)
+            1.2.2) Can add/append/register whenever smthng new calls it
+        1.3) Get data from list in 2.2
+            1.3.1) Writes data to logging file (specify file name for each class)
+        1.4) Create LS thread and update regularly
+*/
+package com.team254.lib.util;
+import java.util.ArrayList;
+
+public class LoggingSystem {
+    // 1.1
+    //  LoggableItems object
+    ArrayList<ILoggable> loggable_items = new ArrayList<ILoggable>();
+    //  creating a usingFileWriter object
+    FileWriter fileWriter = new FileWriter("Test.csv");
+    void LoggingSystem() {
+    }
+    void Register() {  //  start function that opens files
+        try {
+             // creating a loggable object 
+            ILoggable loggable = new TestLoggable();
+            //  adding Loggable to Loggable_items list
+            loggable_items.add(loggable);
+        } catch (Exception e) {}
+
+    }
+    void Log() {  //  function that gets called and told when to log by main
+        try{
+            for (int i=0; i < loggable_items.size(); i++) {
+               ArrayList<Double> items = loggable_items.get(i).get_items();
+               ArrayList<String> item_names = loggable_items.get(i).get_item_names();
+               //  assertArrayEquals(items[i], item_names[i]);
+               //  write to files
+               for (int h=0; h < item_names.size(); h++) {
+               fileWriter.write(item_names.get(h));
+               fileWriter.write(",");
+               }
+               fileWriter.write("\n");
+               for (int j=0; j < items.size(); j++) {
+                fileWriter.write(items.get(j).toString());
+                fileWriter.write(",");
+               } 
+               fileWriter.write("\n");
+            }
+        } catch (Exception e) {}  // making compiler happy by trying to catch stuff that could crash 
+    }
+    void Close() {  //  close file
+        try {
+            fileWriter.close();
+        } catch (Exception e) {}
+
+    }
+    // 1.2
+    public ArrayList<String> canCall;
+    
+}
