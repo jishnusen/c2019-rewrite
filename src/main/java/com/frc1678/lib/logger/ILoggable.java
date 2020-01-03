4fc4cc3b3f86b4593391be37f6a843ec7e247110
EithneA-V <eithne.arsuaga.vazquez@gmail.com>
2020-01-02 16:49:58 -0800

    Fix ILoggable package name

---
+++ src/main/java/com/frc1678/lib/logger/ILoggable.java
@@ -0,0 +1,7 @@
+package com.frc1678.lib.logger;
+import java.util.ArrayList;
+ 
+ public interface ILoggable {
+     ArrayList<ArrayList<Double>> get_items();
+     ArrayList<String> get_item_names();
+ }
